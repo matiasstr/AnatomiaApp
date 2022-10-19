3c6feb8688e988c78dc9452c98650c962a3c0c2d
matiasstr <matias.strficek@gmail.com>
2022-10-19 11:47:58 -0300

    agregado de servidor de backend

---
+++ fisiologia-app/api/index.js
@@ -0,0 +1,10 @@
+const server = require("./App.js");
+const { conn } = require("./src/DB/db.js");
+
+// const PORT = process.env.PORT || 3001;
+
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
+});
