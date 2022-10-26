51563ef3572d26f3b17e25b6f4cb18bbb5604ae1
Alejandro <ale.970@hotmail.com>
2022-10-26 16:41:38 -0300

    Agregado modal con descripcion de img

--- fisiologia-app/client/src/components/Detail/Detail.jsx
+++ fisiologia-app/client/src/components/Detail/Detail.jsx
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import img1 from "./anatomiaslide.png";
-import { AiOutlinePlayCircle } from "react-icons/ai";
+import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { GiSoundWaves } from "react-icons/gi"
 import { obtenerDetalle } from "../../Redux/Actions/Actions";
+import styles from "./Detalle.module.css";
 
 function Detail() {
   const dispatch = useDispatch();
@@ -18,16 +20,42 @@ function Detail() {
 
   return (
     <div>
-      <div className="card w- bg-base-100 shadow-xl image-full">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className={styles.detalleGeneral}>
+        <div className={styles.textoImg}>
+          {/* The button to open modal */}
+          <label htmlFor="my-modal-3" className="btn modal-button">
+            <BsFillJournalBookmarkFill/>
+          </label>
+
+          {/* Put this part before </body> tag */}
+          <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+          <div className="modal">
+            <div className="modal-box relative">
+              <label
+                htmlFor="my-modal-3"
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+              <h3 className="text-lg font-bold">
+                Congratulations random Internet user!
+              </h3>
+              <p className="py-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+                voluptatum, molestias voluptate porro aut autem assumenda
+                perspiciatis doloribus totam obcaecati reiciendis optio a
+                consequuntur architecto non dolorum? Doloribus, vel obcaecati?
+              </p>
+            </div>
           </div>
+        </div>
+        <div >
+          <img className={styles.img_detalle} src={img1} alt="img" />
+        </div>
+        <div>
+          <button>
+            <GiSoundWaves className="h-[5rem] w-[5rem]"/>
+          </button>
         </div>
       </div>
     </div>
