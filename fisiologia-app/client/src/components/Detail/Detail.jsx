7018ab94476bfde151328b96a5a49b9c4d054977
Alejandro <ale.970@hotmail.com>
2022-11-23 17:02:05 -0300

    Obtencion de datos

--- fisiologia-app/client/src/components/Detail/Detail.jsx
+++ fisiologia-app/client/src/components/Detail/Detail.jsx
@@ -11,7 +11,7 @@ import tremendoAudio from "../../assets/media/audio.mp3";
 import styles from "./Detalle.module.css";
 
 function Detail() {
-  let { id } = useParams();
+  // let { id } = useParams();
   const dispatch = useDispatch();
 
   let { id } = useParams();
